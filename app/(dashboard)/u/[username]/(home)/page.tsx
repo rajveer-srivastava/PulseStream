5aac5f645d4cb866bfdb606621edace0a71e063d
Rajveer Srivastava <rajveersrivastava@Rajveers-MacBook-Air.local>
2024-10-25 11:46:50 +0530

    Added tokens to view Stream

--- app/(dashboard)/u/[username]/(home)/page.tsx
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -1,4 +1,26 @@
-const CreatorPage = () => {
-  return <div>Creator Page</div>;
+import { currentUser } from "@clerk/nextjs/server";
+import { getUserByUsername } from "@/lib/user-service";
+import { StreamPLayer } from "@/components/stream-player";
+
+interface CreatorPageProps {
+  params: {
+    username: string;
+  };
+}
+
+const CreatorPage = async ({ params }: CreatorPageProps) => {
+  const externalUser = await currentUser();
+  const user = await getUserByUsername(params.username);
+
+  if (!user || user.externalUserId !== externalUser?.id || !user.stream) {
+    throw new Error("Unauthorized");
+  }
+
+  return (
+    <div className="h-full">
+      <StreamPLayer user={user} stream={user.stream} isFollowing />
+    </div>
+  );
 };
+
 export default CreatorPage;
--- lib/user-service.ts
+++ lib/user-service.ts
@@ -1,9 +1,25 @@
 import { db } from "@/lib/db";
+
 export const getUserByUsername = async (username: string) => {
   const user = await db.user.findUnique({
     where: {
       username,
     },
+    include: {
+      stream: true,
+    },
   });
+
+  return user;
+};
+
+export const getUserByID = async (id: string) => {
+  const user = await db.user.findUnique({
+    where: { id },
+    include: {
+      stream: true,
+    },
+  });
+
   return user;
 };
