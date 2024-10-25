2755ba170135d9833bd1256c769784fe233aa2d4
Rajveer Srivastava <rajveersrivastava@Rajveers-MacBook-Air.local>
2024-10-25 11:27:03 +0530

    Livekit webhook

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,11 +1,15 @@
 "use client";
+
 import { useSidebar } from "@/store/use-sidebar";
 import { User } from "@prisma/client";
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface RecommendedProps {
-  data: User[];
+  data: (User & {
+    stream: { isLive: boolean } | null;
+  })[];
 }
+
 export const Recommended = ({ data }: RecommendedProps) => {
   const { collapsed } = useSidebar((state) => state);
   const showLabel = !collapsed && data.length > 0;
@@ -23,17 +27,18 @@ export const Recommended = ({ data }: RecommendedProps) => {
             key={user.id}
             username={user.username}
             imageUrl={user.imageUrl}
-            isLive={false}
+            isLive={user.stream?.isLive}
           />
         ))}
       </ul>
     </div>
   );
 };
+
 export const RecommendedSkeleton = () => {
   return (
     <ul className="px-2">
-      {[...Array(3)].map((_, i) => (
+      {[...Array(5)].map((_, i) => (
         <UserItemSkeleton key={i} />
       ))}
     </ul>
